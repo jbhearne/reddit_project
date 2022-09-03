4d440c46a727df1aa93df0387d62e35573807590
JBHearne <95382527+jbhearne@users.noreply.github.com>
2022-09-03 15:26:07 -0500

    add Posts component test

--- __tests__/fakeComponent.data.js
+++ __tests__/fakeComponent.data.js
@@ -21,3 +21,48 @@ export const fakeSubredditsComponentData = [
         id: '12345'
     },
 ]
+
+export const fakePostsComponentData = [
+    {
+        author: 'testAuthor',
+        title: 'testTitle',
+        date: 'testDate',
+        upvotes: 1,
+        text: 'testText',
+        images:  ['testImage11', 'testImage12'],
+        media:  { 
+            embed: 'testMedia',
+            video: 'testVideo'
+            },
+        url: 'testUrl',
+        id: '123'
+    },
+    {
+        author: 'testAuthor2',
+        title: 'testTitle2',
+        date: 'testDate2',
+        upvotes: 12,
+        text: 'testText2',
+        images:  ['testImage21', 'testImage22'],
+        media:  { 
+            embed: 'testMedia2',
+            video: 'testVideo2'
+            },
+        url: 'testUrl2',
+        id: '1234'
+    },
+    {
+        author: 'testAuthor3',
+        title: 'testTitle3',
+        date: 'testDate3',
+        upvotes: 13,
+        text: 'testText3',
+        images:  ['testImage31', 'testImage32'],
+        media:  { 
+            embed: 'testMedia3',
+            video: 'testVideo3'
+            },
+        url: 'testUrl3',
+        id: '12345'
+    }
+]
