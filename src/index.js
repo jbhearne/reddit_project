3feea0bfb487cad4d4eb02420767cdf349d98eff
JBHearne <95382527+jbhearne@users.noreply.github.com>
2022-09-18 14:37:02 -0500

    add 200.html for routing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+/*import express from 'express'
+import path from 'path'
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(5000);*/
 
 const container = document.getElementById('root');
 const root = createRoot(container);
